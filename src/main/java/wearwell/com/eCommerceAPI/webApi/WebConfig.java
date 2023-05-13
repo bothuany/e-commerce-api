379022d48ca45134dabb2f8527499888d4ade880
bothuany <rbdikmen@gmail.com>
2023-05-13 04:55:15 +0300

    Web Config is added

---
+++ src/main/java/wearwell/com/eCommerceAPI/webApi/WebConfig.java
@@ -0,0 +1,21 @@
+package wearwell.com.eCommerceAPI.webApi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // your reactjs URL
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
+                .allowedHeaders("Content-Type") // Adjust headers you need to allow
+                .allowCredentials(true); // Add only if you want to access cookie
+    }
+
+}
